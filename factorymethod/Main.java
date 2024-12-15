fa469836d3401bce2e82b939672db97e175eb739
Ismail Lebbar <ismaillebbar@Ismails-MacBook-Pro.local>
2024-12-15 13:39:03 +0100

    Initial commit

---
+++ factorymethod/Main.java
@@ -0,0 +1,11 @@
+package factorymethod;
+
+public class Main {
+    public static void main(String[] args) {
+        Logistics roadLogistics = new RoadLogistics();
+        roadLogistics.planDelivery();
+
+        Logistics seaLogistics = new SeaLogistics();
+        seaLogistics.planDelivery();
+    }
+}
